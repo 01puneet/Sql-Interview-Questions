5e89692b883cc58aacd24096b004d115973431dc
01puneet <sardarpuneetparsadsingh@gmail.com>
2025-12-09 00:38:16 +0530

    updated approach

--- intermediate/Find employees earning more than their department average.sql
+++ intermediate/Find employees earning more than their department average.sql
@@ -9,3 +9,10 @@ group by 1)d
 on e.dept_id=d.dept_id
 )base
 where base.salary>base.avg_department_salary;
+
+-- using window function:
+
+select base.emp_id,base.emp_name, base.dept_id,d.name as department_name, base.salary from (
+select e.emp_id,e.emp_name, e.dept_id,e.salary, avg(salary) over(partition by dept_id) as avg_salary
+  from employees e) base
+  left join departments d
